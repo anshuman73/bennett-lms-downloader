5c9fa1e7d9d0958c506cd4e74db088bbba610bf3
Anshuman Agarwal <dev.anshuman73@gmail.com>
2018-10-15 20:48:51 +0530

    Better methods

--- downloader.py
+++ downloader.py
@@ -2,40 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import urllib
-from selenium.webdriver import Chrome
 from getpass import getpass
-from time import sleep
 
-print('\n\nAfter asking for your credentials, the software will run an automated chrome window to login into the LMS system as you, please do not close it !\n')
-sleep(3)
+
+print('\n\nWe need to ask for you your LMS credentials to login into the LMS system as you so as to retrieve your courses:\n')
 username = input('Enter your bennett username: ')
 password = getpass('Enter your bennett password: ')
 
-driver = Chrome(os.getcwd() + '/' + 'chromedriver')
-driver.get('http://lms.bennett.edu.in')
-username_elem = driver.find_element_by_id('inputName')
-username_elem.clear()
-username_elem.send_keys(username)
-password_elem = driver.find_element_by_id('inputPassword')
-password_elem.clear()
-password_elem.send_keys(password)
-submit_button = driver.find_element_by_id('submit')
-submit_button.click()
+request_session = requests.Session()
+login_request = request_session.post('http://lms.bennett.edu.in/login/index.php?authldap_skipntlmsso=1', data={'username': username, 'password': password, 'submit': ''})
+login_history = login_request.history
+login_history_length = len(login_history)
 
-try:
-	assert driver.current_url == 'http://lms.bennett.edu.in/my/'
-	cookies = driver.get_cookies()
-	cookie = cookies[0]['value']
-	driver.close()
+if login_history_length == 2:
 	local_path = os.getcwd() + '/' + 'Bennett LMS Data' + '/'
 	if not os.path.exists(local_path):
 		os.makedirs(local_path)
-
-	request_session = requests.Session()
-
-	request_session.cookies.set('MoodleSession', cookie)
-
-
 	r = request_session.get('http://lms.bennett.edu.in/my/')
 
 	soup = BeautifulSoup(r.content, 'html.parser')
@@ -57,7 +39,7 @@ try:
 		course_soup = BeautifulSoup(course_data, 'html.parser')
 		resources = course_soup.find_all('li', class_='activity resource modtype_resource ')
 		count = 0
-		for resource in resources:
+		for resource in resources[::-1]:
 			file_url = resource.find('div', class_='activityinstance').find('a')['href']
 			file_req = request_session.get(file_url, stream=True)
 			file_name = urllib.parse.unquote(file_req.url.split('/')[-1])
@@ -84,9 +66,8 @@ try:
 			count += 1
 
 	print('\n\n\nAll Done\n\nKeep studying !')
-except AssertionError:
-	driver.close()
+
+else:
 	print('Invalild Login details ! Please close the window and re-run the program !')
-except Exception as e:
-	driver.close()
-	print(f'Un-identified error {e} occured ! Please let the developer know the exact steps you made')
+
+input('Press Enter to exit')
